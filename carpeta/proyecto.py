e79ef8813d6ed06e469acdc00e45be33d8747bf9
Raque15 <raquelhern.15@gmail.com>
2023-11-18 00:11:40 -0600

    Cambios varios en el metodo

--- carpeta/proyecto.py
+++ carpeta/proyecto.py
@@ -207,7 +207,28 @@ class AnalizadorSemantico:
 
 
 
+            # ----------------------------esto if es de la declaracion de metodos--------------------------------------------
 
+            elif   "(" in palabras and ")" in palabras  :
+                
+                if palabra_actual in tipos_de_datos_llave: 
+
+                    if palabra_actual == "int" and  palabras[ posicion+2]== "(": 
+                            self.tabla_de_simbolos.agregar_funcion(palabras[1],palabras[0])
+                            
+       
+                    elif palabra_actual == "string" and  palabras[ posicion+2]=="(" :    
+                            self.tabla_de_simbolos.agregar_funcion(palabras[1],palabras[0])
+                           
+
+
+                    elif palabra_actual == "float" and  palabras[ posicion+2] =="(": 
+                            self.tabla_de_simbolos.agregar_funcion(palabras[1],palabras[0])
+                            
+
+                  
+                    elif palabra_actual == "void" and  palabras[ posicion+2] =="(": 
+                            self.tabla_de_simbolos.agregar_funcion(palabras[1],palabras[0])
 
     
 
